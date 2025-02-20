5402eceef640843ef5ee13501e9a74bc5eeb92ea
timofei <tima.bratskov05@mail.ru>
2025-02-20 22:39:20 +0300

    all services

---
+++ driver-service/src/main/kotlin/com/example/driver_service/DriverServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.driver_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DriverServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DriverServiceApplication>(*args)
+}
